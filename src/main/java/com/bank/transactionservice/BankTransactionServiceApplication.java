1be683df6732117893a3dbb07a6b14af2abcf6f8
Oliver Castillo <ocastilt@nttdata.com>
2024-11-21 11:28:35 -0500

    chore: initial commit

---
+++ src/main/java/com/bank/transactionservice/BankTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.transactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/transactionservice/BankTransactionServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bank.transactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Add test here
+	}
+
+}
